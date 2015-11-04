f5e933c7d9c38e16ee39a5c6f131ff5c15b848a5
Lugn <tomas.lugn@gmail.com>
2015-11-04 11:02:42 +0100

    Created Demo.ps1 in AzureAutomation.

---
+++ InfraVerktyg/Demo.ps1
@@ -0,0 +1,14 @@
+workflow Verb-Noun 
+{
+    Param
+    (
+        # Param1 help description
+        [string]
+        $Param1,
+
+        # Param2 help description
+        [int]
+        $Param2
+    )
+
+}
